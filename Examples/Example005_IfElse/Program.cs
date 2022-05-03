af01fdfa60781e49977849a35179275309305d61
BARS <makz.ars@gmail.com>
2022-05-03 18:14:41 +0300

    add Example005_IfElse

---
+++ Examples/Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, Маша!");
+}
+else
+{
+    Console.WriteLine("Привет!");
+}
